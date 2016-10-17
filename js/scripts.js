2b6b8425c2879d5b278b2914d3502b341dfdd535
Lina Shadrach and Michelle Llaguno <lina@>
2016-10-17 14:27:32 -0700

    add a function that asks for user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
 
-var g = 10;
-var l = 10;
-var gallonsToLiters = function(gallons){
-  var liters = gallons * 3.78541;
-  return liters;
-}
-alert(gallonsToLiters(g));
-var litersToGallons = function(liters){
-  var gallons = liters / 3.78541;
-  return gallons;
-}
-alert(litersToGallons(l));
+var input = prompt('Enter in the amount you want to convert followed immediately by the unit measurement (g=gallons, l=liter)');
+var inputLength = input.length;
+var unit = input.charAt(inputLength-1);
+var inputSliced = input.slice(0, inputLength-1);
+var inputToInt = parseInt(inputSliced);
+if (unit === "g"){
+  var unit2 = "l";
+  var gallonsToLiters = function(gallons){
+    var liters = gallons * 3.78541;
+    return liters.toFixed(2);
+  };
+  var result = gallonsToLiters(inputToInt);
+};
+if (unit === "l"){
+  var unit2 = "g";
+  var litersToGallons = function(liters){
+    var gallons = liters / 3.78541;
+    return gallons.toFixed(2);
+  };
+  var result = litersToGallons(inputToInt);
+};
+alert(result + unit2 + ".");
